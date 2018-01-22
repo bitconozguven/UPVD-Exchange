414a1cc4704e464c9fd1c541db7fd41991951040
bitconozguven <35693879+bitconozguven@users.noreply.github.com>
2018-01-22 23:01:21 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => (
         <div className="col-xs-12">
           <h2>About Us</h2>
 
-          <p>TEST TEST Nexchange is a fast, reliable and fully transparent cryptocurrency exchange built by crypto enthusiasts, for crypto enthusiasts.</p>
+          <p>TEST TEST Nexchangetest is a fast, reliable and fully transparent cryptocurrency exchange built by crypto enthusiasts, for crypto enthusiasts.</p>
           <p>We are a group of multi-national, multi-disciplinary cryptocurrency professionals.</p>
           <p>Our team came together to make this platform after seeing firsthand the need for a fast and reliable exchange on the crypto market.</p>
           <p>We pride ourselves on total transparency: Everything is viewable through our API, starting from order lists and ending with our coin reserves. </p>
